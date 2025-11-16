a1c3015c70d057068394310c9cb8eb0bc53525c9
c5035565 <160241967@student.rnngroup.ac.uk>
2025-11-16 23:00:35 +0000

    Trying to store the login information  which is still a work in progress!

--- My_Application/Program.cs
+++ My_Application/Program.cs
@@ -2,58 +2,27 @@
 
 using My_Application;
 using System;
+using System.Security.Cryptography.X509Certificates;
 
 
 class Program
 {
     static void Main(string[] args)
     {
-
-        register reg = new register("", "", "", "");
-
-        Console.WriteLine("Welcome to to your food and reciepe manager...");
-
+        application_manager manager = new application_manager(); 
+        manager.startApp(); 
        
-        string choice;
-
-        do
-        {
-            Console.WriteLine("Do you want to log in or register? ('l' to login /'r' to register)");
-        
-            choice = Console.ReadLine().ToLower();
 
 
-            if (choice == "l")
-            {
-                Console.WriteLine("Login selected.");
-            }
-            else if (choice == "r")
-            {
-                reg.Register();
-                mainMenu();
-            }
-            else
-            {
-                Console.WriteLine("Invalid choice. Please enter 'l' to login or 'r' to register.");
-            }
-        }
-       
-        while (choice != "l" && choice != "r");
-
-        
-    } 
 
 
-    public static void mainMenu()
-    {
-        Console.WriteLine("______________Main Menu______________");
-        Console.WriteLine("1. View Recipes");
-        Console.WriteLine("2. Add Recipe");
-        Console.WriteLine("3. View Ingredients");
-        Console.WriteLine("4. Add Ingredient"); 
-        Console.WriteLine("5. Logout");
+
     }
-} 
+}
+
+
+    
+
 
 
 
--- My_Application/register.cs
+++ My_Application/register.cs
@@ -7,44 +7,142 @@ using System.Threading.Tasks;
 
 namespace My_Application
 {
-     class register
-    { 
-        private string _username; 
-        private string _password;
-        private string _email; 
-        private string _phoneNumber;
+    public class application_manager
+    {
+        register reg = new register("", "", "", "");
 
-        public register(string username, string password,string email, string phonenumber) 
+        public void startApp()
         { 
-            this._username = username; 
-            this._password = password; 
-            this._email = email; 
-            this._phoneNumber = phonenumber;
+            
+            string choice;
 
+            do
+            {
+                Console.WriteLine("Do you want to log in or register? ('l' to login /'r' to register)");
+
+                choice = Console.ReadLine().ToLower();
+                login login = new login();
+                bool loggedin = false;
+
+
+                if (choice == "l")
+                {
+
+                    if (login.Login(reg.Username, reg.Password))
+                    {
+                        loggedin = true;
+                    }
+
+                }
+                else if (choice == "r")
+                {
+                    reg.Register();
+                    mainMenu();
+                }
+                else
+                {
+                    Console.WriteLine("Invalid choice. Please enter 'l' to login or 'r' to register.");
+                }
+            }
+
+            while (choice != "l" && choice != "r");
         }
-        public  void Register()
+
+        public void mainMenu()
         {
-            Console.WriteLine("Register selected");
-            
-            Console.WriteLine("Please enter your username");
-            string username = Console.ReadLine();
-            
-            Console.WriteLine("Please enter your password");
-            string password = Console.ReadLine(); 
-            
-            Console.WriteLine("Please enter your email"); 
-            string email = Console.ReadLine(); 
-            
-            Console.WriteLine("Please enter your phone number");     
-            string phonenumber = Console.ReadLine();
-            
-            Console.WriteLine($"Welcome {username}\nYou can start tracking your reciepies"); 
-            
+            Console.WriteLine("______________Main Menu______________");
+            Console.WriteLine("1. View Recipes");
+            Console.WriteLine("2. Add Recipe");
+            Console.WriteLine("3. View Ingredients");
+            Console.WriteLine("4. Add Ingredient");
+            Console.WriteLine("5. Logout");
+            string  choice =  Console.ReadLine();
 
-            return;
+            if (choice == "5")
+            {
+                startApp();
+            }
+            else if(choice=="1" && choice == "2" && choice == "3" && choice =="4")
+            {
+                Console.WriteLine("Feature not implemented yet.");
+                mainMenu();
+            }
+            else
+            {
+                Console.Write("Break");
+            }
 
 
         }
-    } 
 
-} 
+        public class register
+        { 
+            public string Username { get { return _username; } }  
+            public string Password { get { return _password; } }
+            private string _username;
+            private string _password;
+            private string _email;
+            private string _phoneNumber;
+
+            public register(string username, string password, string email, string phonenumber)
+            {
+                this._username = username;
+                this._password = password;
+                this._email = email;
+                this._phoneNumber = phonenumber;
+
+            }
+            public void Register()
+            {
+                Console.WriteLine("Register selected");
+
+                Console.WriteLine("Please enter your username");
+                this._username = Console.ReadLine();
+
+                Console.WriteLine("Please enter your password");
+                this._password = Console.ReadLine();
+
+                Console.WriteLine("Please enter your email");
+                this._email = Console.ReadLine();
+
+                Console.WriteLine("Please enter your phone number");
+                this._phoneNumber = Console.ReadLine();
+
+                Console.WriteLine($"Welcome {this._username}\nYou can start tracking your reciepies");
+                
+                application_manager.startapp();
+
+
+                return;
+
+
+            }
+        }
+        public class login
+        {
+            public bool Login(string stored_username, string stored_password)
+            {
+
+                Console.WriteLine("Login selected");
+                Console.WriteLine("Please enter your username");
+                string enteredUsername = Console.ReadLine();
+                Console.WriteLine("Please enter your password");
+                string enteredPassword = Console.ReadLine();
+
+                if (enteredUsername == stored_username && enteredPassword == stored_password)
+                {
+                    Console.WriteLine($"Welcome back {stored_username}\nYou can start tracking your reciepies");
+                    return true;
+                }
+                else
+                {
+                    Console.WriteLine("Invalid username or password. Please try again.");
+                    return false;
+                }
+
+
+
+            }
+        }
+    }
+}
